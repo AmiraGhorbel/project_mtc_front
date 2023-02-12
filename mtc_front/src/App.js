6e9b057b088f00fc9569706e7289da225fbf57ad
AmiraGhorbel <amiraghorbel72@gmail.com>
2023-02-12 22:37:34 +0100

    Third commit

--- mtc_front/src/App.js
+++ mtc_front/src/App.js
@@ -3,6 +3,9 @@ import AddFiles from './components/AddFiles';
 import './App.css';
 import ListFiles from './components/ListFiles';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
+import LogOut from './components/LogOut/LogOut';
 function App() {
   return (
     <div className="App">
@@ -10,6 +13,9 @@ function App() {
       <Routes>
           <Route path="/" element={<AddFiles />} />
           <Route path="/liste" element={<ListFiles />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/logout" element={<LogOut />} />
           </Routes>
       </Router>
     </div>
@@ -22,3 +28,4 @@ export default App;
 //package 3: npm i @react-pdf-viewer/default-layout
 //package 4: npm i react-router-dom
 //package 5: npm install bootstrap@5.3.0-alpha1
+//signIn et loggOut
